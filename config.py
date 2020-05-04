abdf0aa306d896a55055ff31cf42904e77706183
hemostasisknowledge <hemostasisknowledge@gmail.com>
2020-05-04 21:10:29 +0300

    "init"

---
+++ config.py
@@ -0,0 +1,1 @@
+token = "1001349502:AAH1pQlC9nshDhO_7hLb2eW5XLoUrda-MzI"
---
+++ weatherbot.py
@@ -0,0 +1,26 @@
+import config
+import telebot
+import pyowm
+
+owm = pyowm.OWM('dd1b79b9daf5ef14480da869906d25fb', Language = "ru")
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+    observation = owm.weather_at_place(message.text)
+    w = observation.get_weather()
+    temp = w.get_temperature('celsius')["temp"]
+
+    answer = "In the forest of " + message.text + "there is " + w.get_detailed_status() + "\n"
+    answer+= "The temperature is around " + str(temp) + "\n\n"
+
+    if temp < 0:
+        answer += "It's cold, warm up!"
+    elif temp < 10:
+        answer += "It's OK for fur bubbles"
+    else:
+        answer += "Such a nice day!"
+
+    bot.send_message(message.chat.id, answer)
+
+bot.polling( none_stop = True )
